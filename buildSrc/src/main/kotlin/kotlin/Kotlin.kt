c7dff0319d9c488f69286d0500875eb2113c96c6
RobertoMartinezCS <roberto.mtz.med.cs@gmail.com>
2021-03-11 00:00:50 -0600

    feat: add buildsrc config

---
+++ buildSrc/src/main/kotlin/kotlin/Kotlin.kt
@@ -0,0 +1,15 @@
+/** */
+object Kotlin {
+
+    /* */
+    private object Version {
+
+        /* @see [https://kotlinlang.org/docs/whatsnew1430.html] */
+        const val kotlinVersion = "1.4.30"
+
+    }
+
+    /* @see [https://kotlinlang.org/docs/reference/android-overview.html] */
+    const val standardLibrary = "org.jetbrains.kotlin:kotlin-stdlib:${Version.kotlinVersion}"
+
+}
