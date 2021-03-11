ffaac005c86a14e63f84ff42482ba27936be2cc6
RobertoMartinezCS <roberto.mtz.med.cs@gmail.com>
2021-03-10 23:15:39 -0600

    feat: initial commit

---
+++ app/src/main/java/cs/roberto/punkbeers/MainActivity.kt
@@ -0,0 +1,11 @@
+package cs.roberto.punkbeers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
